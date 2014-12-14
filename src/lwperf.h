dfdba6cf4d28a218446425516123c3f4ea55a557
Eric Anger <eanger@gmail.com>
2014-12-14 11:41:35 -0500

    rename some files

--- src/lwperf.h
+++ src/lwperf.h
@@ -1,34 +1,44 @@
-#ifndef lwperf_h
-#define lwperf_h
+#pragma once
 
-#ifndef __cplusplus
-#error "this file is for inclusion in C++ sources only"
+#include "perf_api_impl.h"
+
+#ifdef __cplusplus
+extern "C" {
 #endif
 
-#if defined(_USE_EIGER) || defined(_USE_CSV) || defined(_USE_EIGER_MODEL) || \
-    defined(_USE_FAKEEIGER)
+typedef struct LWPERF_IMPL* lwperf_t;
 
-#include "perf.h"
+inline lwperf_t lwperf_init(const char* machine, const char* application,
+                            const char* dbname, const char* prefix,
+                            const char* suffix) {
+  return lwperf_init_impl(machine, application, dbname, prefix, suffix);
+}
+inline void lwperf_finalize(lwperf_t perf) { lwperf_finalize_impl(perf); }
+inline void lwperf_add_invariant(lwperf_t perf, const char* name,
+                                 double value) {
+  lwperf_add_invariant_impl(perf, name, value);
+}
+inline void lwperf_add_cite_param(lwperf_t perf, const char* cite_name,
+                                  const char* param_name, double value) {
+  lwperf_add_cite_param_impl(perf, cite_name, param_name, value);
+}
+inline void lwperf_log(lwperf_t perf, const char* cite_name) {
+  lwperf_log_impl(perf, cite_name);
+}
+inline void lwperf_stop(lwperf_t perf, const char* cite_name) {
+  lwperf_stop_impl(perf, cite_name);
+}
+/* 
+ * This function can take a variable number of name--value pairs as arguments.
+ * For example:
+ * lwperf_add_cite_params(perf, cite_name, name_1, value_1, name_2, value_2, ...)
+ * Where the parameter types are
+ *    const char *name_1, *name_2;
+ *    double value_1, value_2;
+ */
+#define lwperf_add_cite_params(perf, cite_name, ...) \
+  lwperf_add_cite_params_impl(perf, cite_name, __VA_ARGS__)
 
-#else // PERF_DISABLE is set
-#define PERF ErRoR
-#define PERFFORMATTER NO_LWPERF_ENABLED
-#define PERFINIT
-#define PERFFINALIZE
-#define PERFMPIARGS
-#define PERFDECL(...)
-#define PERFLOG(X, ...)
-#define PERFLOGKEEP(X, ...)
-#define PERFSTART(X)
-#define PERFSTOP(X, ...) 
-#define PERFSTOPKEEP(X, ...) 
-#define REGISTERINVARIANT(name,value)
-#define DR(v)
-#define DD(v)
-#define DN(v)
-#define IR(v)
-#define ID(v)
-#define IN(v)
-#endif // PERF_DISABLE
-
-#endif // lwperf_h
+#ifdef __cplusplus
+} // extern "C"
+#endif
